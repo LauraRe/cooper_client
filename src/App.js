fb4fd18b7e4c1508765d80ba439ef721e3a7911c
olivia <olivia@evolve.se>
2019-03-06 10:52:46 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import DisplayCooperResult from './Components/DisplayCooperResult';
+import InputFields from './Components/InputFields';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      distance: '',
+      gender: 'female',
+      age: ''
+    }
+  }
+
+  onChange(event) {
+    this.setState({
+      [event.target.id]: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <InputFields 
+        inputChangeHandler={this.onChange.bind(this)}
+        />
+        
+        <DisplayCooperResult
+          distance={this.state.distance}
+          gender={this.state.gender}
+          age={this.state.age}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
